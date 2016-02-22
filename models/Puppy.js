70208ece1a2c9b64fd4325a648ccd64b55f8c2bc
Darien82 <DarienCurry@gmail.com>
2016-02-21 19:22:22 -0800

    Puppy Functions

---
+++ models/Puppy.js
@@ -0,0 +1,28 @@
+var mongoose    = require('mongoose');
+var Schema      = mongoose.Schema
+
+// REQUIRE USER MODEL
+
+var User        = require('./User');
+
+
+// DEFINE PUPPY SCHEMA
+
+var PuppySchema = new mongoose.Schema({
+  name:           String,
+  age:            Date,
+  therapy:        Boolean,
+  friendliness:   String,
+  hypoallergenic: Boolean,
+  url:            String,
+  size:           String,
+  user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+  }
+});
+
+//||||||||||||||||||||||||||--
+// EXPORT SCHEMA
+//||||||||||||||||||||||||||--
+module.exports = mongoose.model('Puppy', PuppySchema);
